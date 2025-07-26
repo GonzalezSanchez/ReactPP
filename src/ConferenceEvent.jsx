a5edb3a54599ea4c582bda46002b696cbc5834ec
AGS <a_gonzalez_sanchez@hotmail.com>
2025-07-26 00:09:33 -0400

    the balnk space in venueSlice for the name of Auditoriuum

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -69,12 +69,12 @@ const ConferenceEvent = () => {
     return (
         <>
             <navbar className="navbar_event_conference">
-                <div className="company_logo">Conference Expense Planner</div>
+                <div className="company_logo">Conference Expense Plannerxxx</div>
                 <div className="left_navbar">
                     <div className="nav_links">
-                        <a href="#venue" onClick={() => navigateToProducts("#venue")} >Venue</a>
-                        <a href="#addons" onClick={() => navigateToProducts('#addons')}>Add-ons</a>
-                        <a href="#meals" onClick={() => navigateToProducts('#meals')}>Meals</a>
+                        <a href="#venue" onClick={() => navigateToProducts("#venue")} >Venuexxx</a>
+                        <a href="#addons" onClick={() => navigateToProducts('#addons')}>Add-onsxxx</a>
+                        <a href="#meals" onClick={() => navigateToProducts('#meals')}>Mealsxxx</a>
                     </div>
                     <button className="details_button" onClick={() => setShowItems(!showItems)}>
                         Show Details
